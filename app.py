039c2a647009e3dd664e2e6e77b0ae365b4f1b1a
ahmadinho22-collab <ahmadinho22@gmail.com>
2026-04-17 20:26:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if not api_key:
 
 # إعداد Gemini
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 # واجهة المحادثة
 if "messages" not in st.session_state:
